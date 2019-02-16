7752979fd004ca9415d6f4e166d0d28b649cddca
sx14 <1059363093@qq.com>
2019-02-16 12:00:37 +0800

    ..

--- lib/model/faster_rcnn/faster_rcnn.py
+++ lib/model/faster_rcnn/faster_rcnn.py
@@ -36,7 +36,7 @@ class _fasterRCNN(nn.Module):
         self.grid_size = cfg.POOLING_SIZE * 2 if cfg.CROP_RESIZE_WITH_MAX_POOL else cfg.POOLING_SIZE
         self.RCNN_roi_crop = _RoICrop()
 
-    def forward(self, im_data, im_info, gt_boxes, num_boxes, use_rpn=False):
+    def forward(self, im_data, im_info, gt_boxes, num_boxes, use_rpn=True):
         batch_size = im_data.size(0)
 
         im_info = im_info.data

ae16af123b09de0b0fe32c4a12bb8ab942abcee1
sx14 <1059363093@qq.com>
2019-03-02 21:24:01 +0800

    fix: out of memory

--- lib/model/heir_rcnn/hier_rcnn.py
+++ lib/model/heir_rcnn/hier_rcnn.py
@@ -53,6 +53,8 @@ class _HierRCNN(nn.Module):
         # label vectors
         with h5py.File(label_vec_path, 'r') as f:
             label_vecs = np.array(f['label_vec'])
+
+        with torch.autograd.no_grad:
             self.label_vecs = Variable(torch.from_numpy(label_vecs).float()).cuda()
 
         # visual embedding vector length

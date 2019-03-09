bb6e136a5cf298991771ce47d8a665c4b5c393d9
sx14 <1059363093@qq.com>
2019-03-09 10:45:00 +0800

    ..

--- lib/model/hier_utils/tree_infer.py
+++ lib/model/hier_utils/tree_infer.py
@@ -10,7 +10,9 @@ from torch.nn.functional import softmax
 
 class TreeNode:
     def __init__(self, name, index, info_ratio):
-        self._cond_prob = -1
+        self._cond_prob = None
+        self._raw_score = None
+        self._prob = None
         self._name = name
         self._index = index
         self._parents = []
@@ -19,6 +21,12 @@ class TreeNode:
 
     def __str__(self):
         return '%s[%.2f]' % (self._name, self.score())
+
+    def raw_score(self):
+        return self._raw_score
+
+    def set_raw_score(self, raw_score):
+        self._raw_score = raw_score
 
     def depth(self):
         min_p_depth = 0
@@ -35,18 +43,20 @@ class TreeNode:
     def append_parent(self, parent):
         self._parents.append(parent)
 
-    def set_cond_prob(self, score):
-        self._cond_prob = score
+    def set_cond_prob(self, cond_prob):
+        self._cond_prob = cond_prob
 
     def cond_prob(self):
         return self._cond_prob
 
     def prob(self):
-        p = self.cond_prob()
-        curr = self
-        while len(curr._parents) > 0:
-            p *= curr._parents[0].cond_prob()
-            curr = curr._parents[0]
+        if self._prob is None:
+            p = self.cond_prob()
+            curr = self
+            while len(curr._parents) > 0:
+                p *= curr._parents[0].cond_prob()
+                curr = curr._parents[0]
+            self._prob = p
         return p
 
     def info_ratio(self):
@@ -58,7 +68,8 @@ class TreeNode:
     def entropy(self):
         e = 0.0
         for c in self._children:
-            e -= c.cond_prob() * log(c.cond_prob, len(self._children))
+            e -= c.cond_prob() * log(c.cond_prob(), len(self._children))
+        return e
 
     def index(self):
         return self._index
@@ -70,11 +81,13 @@ class TreeNode:
 
 def construct_tree(labelnet, scores):
     ind2node = dict()
+    # node meta info
     for label in labelnet.get_all_labels():
         hnode = labelnet.get_node_by_name(label)
         tnode = TreeNode(label, hnode.index(), hnode.info_ratio(labelnet.pos_leaf_sum()))
         ind2node[hnode.index()] = tnode
 
+    # node hierarchy
     for label in labelnet.get_all_labels():
         hnode = labelnet.get_node_by_name(label)
         tnode = ind2node[hnode.index()]
@@ -84,13 +97,14 @@ def construct_tree(labelnet, scores):
             pnode.add_children(tnode)
             tnode.append_parent(pnode)
 
+    # node raw score
     ranked_inds = np.argsort(scores)[::-1]
     for r, ind in enumerate(ranked_inds):
         # rank = r + 1.0  # 1 based
         # score = (len(ranked_inds) - rank) / len(ranked_inds)
         score = scores[ind]
         tnode = ind2node[ind]
-        tnode.set_cond_prob(score.tolist())
+        tnode.set_raw_score(score.tolist())
 
     return ind2node
 
@@ -114,24 +128,28 @@ def good_thresh(max_depth, depth):
 def top_down_search(root, max_depth, threshold=0):
     root.set_cond_prob(1.0)
     node = root
-    print('\t\tP0\t\tP1\t\tE\t\tI')
+    print('P0\t\tP1\t\tE\t\tI')
     while len(node.children()) > 0:
-        print('%s:\t(%.2f)\t\t(%.2f)\t\t(%.2f)\t\t(%.2f)' % (node.name(), node.prob(), node.cond_prob(), node.entropy(), node.info_ratio()))
-
-
         c_scores = []
         for c in node.children():
-            c_scores.append(c.cond_prob())
+            c_scores.append(c.raw_score())
         c_scores_v = Variable(Tensor(c_scores))
         c_scores_s = softmax(c_scores_v, 0)
+
+        for i, c in enumerate(node.children()):
+            c.set_cond_prob(c_scores_s[i].data.numpy().tolist())
+
         pred_c_ind = torch.argmax(c_scores_s)
         pred_c_scr = c_scores_s[pred_c_ind]
 
         threshold = good_thresh(node.depth(), max_depth)
         if pred_c_scr < threshold:
             break
+
+        print('(%.2f)\t(%.2f)\t(%.2f)\t(%.2f) %s' % (node.prob(), node.cond_prob(), node.entropy(), node.info_ratio(), node.name()))
+
         node = node.children()[pred_c_ind]
-        node.set_cond_prob(pred_c_scr.data.numpy().tolist())
+    print('(%.2f)\t(%.2f)\t(%.2f)\t(%.2f) %s' % (node.prob(), node.cond_prob(), node.entropy(), node.info_ratio(), node.name()))
     return node
 
 

24c02600616d10ba42a346b2350180ca6a3039bf
sx14 <1059363093@qq.com>
2019-03-13 00:59:30 +0800

    ..

---
+++ lib/datasets/vg200/process/__init__.py
@@ -0,0 +1,15 @@
+from nltk.corpus import wordnet as wn
+
+
+# syns = wn.synsets('disk.n.01')
+#
+# for syn in syns:
+#     print('============')
+#     print(syn.name()+' : '+syn.definition())
+#     for h in syn.hypernym_paths():
+#         print(h)
+
+#
+# syn = wn.synset('dog.n.01')
+# for h in syn.hypernym_paths():
+#     print(h)

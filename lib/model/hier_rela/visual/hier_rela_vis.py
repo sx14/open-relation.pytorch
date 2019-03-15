7522641e8b68f8e84d7d38746670b0fb89e07e74
sx14 <1059363093@qq.com>
2019-03-15 13:09:35 +0800

    ..

--- lib/model/hier_rela/visual/hier_rela_vis.py
+++ lib/model/hier_rela/visual/hier_rela_vis.py
@@ -115,16 +115,13 @@ class _HierRelaVis(nn.Module):
         sbj_obj_boxes = torch.cat([sbj_boxes, obj_boxes], 1)
 
         with torch.no_grad():
-            sbj_obj_pooled_feat, \
-            sbj_obj_embedding = self._hierRCNN.ext_feat(im_data, im_info,
+            sbj_obj_pooled_feat = self._hierRCNN.ext_feat(im_data, im_info,
                                                         sbj_obj_boxes,num_boxes * 2,
                                                         use_rpn=False)
 
         num_boxes_padding = gt_boxes.size(1)
         sbj_pooled_feat = sbj_obj_pooled_feat[:num_boxes_padding, :]
         obj_pooled_feat = sbj_obj_pooled_feat[num_boxes_padding:, :]
-        sbj_embedding = sbj_obj_embedding[:num_boxes_padding, :]
-        obj_embedding = sbj_obj_embedding[num_boxes_padding:, :]
 
         # ===== class prediction part =====
         # ===== order embedding here =====\

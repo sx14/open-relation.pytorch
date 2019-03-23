e7b8fe51abc1334152bc8635478109cf0de280e0
sx14 <1059363093@qq.com>
2019-03-23 14:10:31 +0800

    ..

--- hier_det/show_det.py
+++ hier_det/show_det.py
@@ -3,11 +3,11 @@ import pickle
 import cv2
 import numpy as np
 from hier_det.show_box import show_boxes
-from global_config import VRD_ROOT
+from global_config import VRD_ROOT, PROJECT_ROOT
 from lib.datasets.vrd.label_hier.obj_hier import objnet
 
 dataset = 'vrd'
-det_roidb_path = 'det_infer_roidb_vrd.bin'
+det_roidb_path = os.path.join(PROJECT_ROOT, 'hier_rela', 'det_roidb_vrd.bin')
 det_roidb = pickle.load(open(det_roidb_path))
 
 
